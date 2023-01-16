d5a08acca72e9ccd872294cef4d6134de582b6d1
v.peskarski <v.peskarskij@gmail.com>
2023-01-16 16:28:17 +0100

    fix: fix url matching

--- src/shared/helpers/urlChecker.ts
+++ src/shared/helpers/urlChecker.ts
@@ -2,8 +2,14 @@ const isUrlMatch = (requestUrl: string, routeUrl: string) => {
   const requestUrlSegments = requestUrl.split('/');
   const routeUrlSegments = routeUrl.split('/');
 
-  const isMatch = requestUrlSegments.length === routeUrlSegments.length
+  let isMatch = false;
+
+  if (requestUrlSegments.length === 4) {
+    isMatch = requestUrlSegments.length === routeUrlSegments.length
     && requestUrl.startsWith(routeUrl);
+  } else {
+    isMatch = requestUrl === routeUrl;
+  }
 
   return isMatch;
 };
