7c01175635ac925af509628e9e640e3fe8c33521
v.peskarski <v.peskarskij@gmail.com>
2023-01-16 15:20:50 +0100

    feat: implement multi server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import Application from './shared/application';
 import userRouter from './components/users/router';
 
+const isMulti = process.argv.includes('--multi');
+
 const application = new Application(userRouter);
 
-application.createServer();
+if (isMulti) {
+  application.createMultiServer();
+} else {
+  application.createServer();
+}
--- src/shared/application.ts
+++ src/shared/application.ts
@@ -1,13 +1,18 @@
 import {
-  createServer, IncomingMessage, Server, ServerResponse,
+  createServer, IncomingMessage, Server, ServerResponse, request,
 } from 'http';
 import * as dotenv from 'dotenv';
+import { cpus } from 'node:os';
+import cluster from 'node:cluster';
 import Router from './router';
 import resolveAction from './helpers/actionResolver';
 
 dotenv.config();
 
-const port = process.env.API_PORT;
+const mainPort = Number(process.env.API_PORT);
+let requestIteration = 0;
+
+const cpusLength = cpus().length;
 
 class Application {
   router: Router;
@@ -18,16 +23,55 @@ class Application {
 
   appServer: Server<typeof IncomingMessage, typeof ServerResponse> | undefined;
 
-  createServer() {
+  createServer(portNumber = mainPort) {
     this.appServer = createServer(async (req, res) => {
       const routes = this.router.getRoutes();
 
       resolveAction(req, res, routes);
     });
 
-    this.appServer.listen(port, () => {
-      console.log(`server started on port: ${port}`);
+    this.appServer.listen(portNumber, () => {
+      if (portNumber === mainPort) {
+        console.log(`server started on port: ${mainPort}`);
+      }
     });
+  }
+
+  createMultiServer() {
+    if (cluster.isPrimary) {
+      for (let cpuIndex = 0; cpuIndex < cpusLength; cpuIndex += 1) {
+        cluster.fork();
+      }
+
+      cluster.on('exit', () => {
+        cluster.fork();
+      });
+
+      this.appServer = createServer(async (req, res) => {
+        const clusterPort = this.getNextPort();
+
+        const routes = this.router.getRoutes();
+
+        resolveAction(req, res, routes);
+
+        req.pipe(request({ port: clusterPort }, (response) => {
+          response.pipe(res);
+        }));
+      });
+
+      this.appServer.listen(mainPort, () => {
+        console.log(`server started on port: ${mainPort}`);
+      });
+    } else {
+      const clusterPort = mainPort + (cluster.worker?.id ?? 0);
+      this.createServer(clusterPort);
+    }
+  }
+
+  getNextPort() {
+    requestIteration = requestIteration === cpusLength ? 1 : requestIteration + 1;
+
+    return mainPort + requestIteration;
   }
 
   getServer() {
